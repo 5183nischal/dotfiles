1561d0c2236c6781746b7f638296fae17174b58c
Nisch <nm2508@nyu.edu>
2023-02-24 15:14:54 +0200

    Initial commit

---
+++ ltex/config.lua
@@ -0,0 +1,35 @@
+return {
+  dictionary = {
+    ["en-US"] = {
+      "MLSys",
+      "DNN",
+      "DNNs",
+      "NSDI",
+      "OSDI",
+      "SOSP",
+      "ISCA",
+      "HPCA",
+      "ASPLOS",
+      "ICLR",
+      "NeurIPS",
+      "ICML",
+      "CVPR",
+      "AAAI",
+      "EMNLP",
+      "USENIX",
+      "ImageNet",
+      "tradeoff",
+      "Jae-Won",
+      "MLOps",
+      "KubeFlow",
+      "DeepSpeed",
+      "Megatron",
+      "microbatch",
+      "docstring",
+      "Mosharaf",
+      "Huggingface",
+      "MAB",
+      "CloudLab",
+    }
+  }
+}
